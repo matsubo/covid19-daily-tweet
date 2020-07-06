3530746a41eb3b207669154df28936cd83f28715
Yuki Matsukura <matsubokkuri@gmail.com>
2020-07-07 06:47:33 +0900

    refactoring

--- covid_tweet.rb
+++ covid_tweet.rb
@@ -4,6 +4,7 @@
 class CovidTweet
   require 'net/http'
   require 'active_support/all'
+  require 'tempfile'
 
   DOWNLOAD_DIR = 'downloads'
 
@@ -16,10 +17,8 @@ class CovidTweet
     # 設定を読み込む
     yaml = YAML.load_file('settings.yaml')
 
-    accounts = yaml['accounts']
-
     threads = []
-    accounts.each do |account|
+    yaml['accounts'].each do |account|
       threads << Thread.new do
         request_and_tweet(account, base_day)
       end
@@ -33,59 +32,39 @@ class CovidTweet
   def request_and_tweet(account, base_day)
     # 設定を取得
     prefecture = account['prefecture']
+    prefecture_ja = account['prefecture_ja']
     url = account['csv']
     twitter = account['twitter']
     column_index = account['column'].to_i
     date_format = account['date']
     encoding = account['encoding']
 
-    puts prefecture
-    puts account
-
     # 終日回す
-    @logger.info("start thread for area: #{prefecture}")
+    @logger.info("[#{prefecture}] started.")
     loop do
-      @logger.info("[#{prefecture}] run at time:" + Time.now.to_s)
+      if File.exist?(archive_file(prefecture, base_day))
+        @logger.info("[#{prefecture}] Waiting until tomorrow morning.")
+        sleep((1.day.since.midnight + 8.hour).to_i - Time.now.to_i)
+      end
 
-      # 一時保存用ファイル名設定
-      file_path = ''
-
+      tempfile = nil
       Retriable.retriable do
-        file_path = download(url, get_todays_filename(prefecture))
+        tempfile = download(url)
       end
 
       # Check the today's data is updated
-      results = analyze_csv(file_path, column_index, base_day, date_format, encoding)
+      results = analyze_csv(tempfile, column_index, base_day, date_format, encoding)
 
-      # Wait for a while until next check
-      next_run_wait_seconds = 30.minutes.to_i
+      @logger.info("[#{prefecture}] Parse result: #{results}")
 
       # Tweet if today's data is updated.
       if results[:base_day_count].positive? && results[:prev_day_count].positive?
-        @logger.info("[#{prefecture}] end today run at time:" + Time.now.to_s)
-
-        # 次回は翌日の8時から実行する
-        next_run_wait_seconds = (1.day.since.midnight + 8.hour).to_i - Time.now.to_i
-        diff = results[:base_day_count] - results[:prev_day_count]
-        percent = (diff * 100 / results[:prev_day_count]).abs.to_i.to_s + '%'
-
-        signal = get_signal(diff)
-
-        message = format('本日の新規陽性者数は%s人です。（前日比 %s%s人,%s%s） #covid19 #%s', results[:base_day_count], signal, diff.abs, signal, percent, prefecture)
-
-        @logger.info(message)
-
-        tweet(message, twitter)
-
-        # ファイルが存在する場合は名前を変更する
-        File.rename(file_path, File.join(DOWNLOAD_DIR, get_todays_filename(prefecture))) if File.exist?(file_path)
-      else
-        # ファイルが存在する場合は削除する
-        File.delete(file_path) if File.exist?(file_path)
+        tweet(twitter, get_message(prefecture_ja, results[:base_day_count], results[:prev_day_count]))
+        File.rename(tempfile, archive_file(prefecture, base_day))
       end
 
-      @logger.info("[#{prefecture}] wait seconds for next run:" + next_run_wait_seconds.to_s)
-      sleep(next_run_wait_seconds)
+      @logger.info("[#{prefecture}] Sleeping 30 min")
+      sleep(30.minutes.to_i)
     end
   end
 
@@ -101,32 +80,26 @@ class CovidTweet
 
   private
 
-  # ファイルをダウンロードする
-  def download(url, file_name)
-    download_url = url
-    # HTTP定義を行う
-    download_uri = URI.parse(download_url)
+  #
+  # Download and return saved File object
+  #
+  # @return File
+  #
+  def download(url)
+    download_uri = URI.parse(url)
     http = Net::HTTP.new(download_uri.host, download_uri.port)
     http.use_ssl = true
     req = Net::HTTP::Get.new(download_uri.path)
 
-    @logger.info("downloading url : #{download_url}")
+    @logger.info("downloading file: #{url}")
 
-    # ダウンロードをリクエストする
     response = http.request(req)
 
-    # 保存先パスを定義
-    file_path = File.join(DOWNLOAD_DIR, file_name)
+    tempfile = Tempfile.create
 
-    # ファイルが存在する場合は削除する
-    File.delete(file_path) if File.exist?(file_path)
+    File.write(tempfile, response.body)
 
-    # ファイルを保存する
-    File.write(file_path, response.body)
-
-    @logger.info("downloaded url : #{download_url}")
-
-    file_path
+    tempfile
   end
 
   # CSVファイルを読み込み、基準日と前日の人数を取得する
@@ -136,8 +109,6 @@ class CovidTweet
     # get previous date
     prev_day = base_date.prev_day
     prev_day_str = prev_day.strftime(date_format)
-
-    @logger.info("analyze csv file with date: #{base_date_str} and #{prev_day_str}")
 
     prev_day_count = 0
     base_day_count = 0
@@ -156,7 +127,7 @@ class CovidTweet
         end
       end
     end
-    @logger.info("analyze csv file end: #{base_day_count}, #{prev_day_count}")
+
     {
       base_day_count: base_day_count,
       prev_day_count: prev_day_count
@@ -164,18 +135,32 @@ class CovidTweet
   end
 
   # ツイートする
-  def tweet(message, twitter)
+  def tweet(twitter, message)
     client = Twitter::REST::Client.new do |config|
       config.consumer_key = twitter['consumer_key']
       config.consumer_secret = twitter['consumer_secret']
       config.access_token = twitter['access_token']
       config.access_token_secret = twitter['access_token_secret']
     end
+
+    @logger.info(message)
     client.update(message)
   end
 
+  def archive_file(prefecture, _base_day)
+    File.join(DOWNLOAD_DIR, prefecture + Time.now.strftime('%Y%m%d') + '.csv')
+  end
 
-  def get_todays_filename(prefecture)
-    prefecture + Time.now.strftime('%Y%m%d') + '.csv'
+  def get_message(prefecture, base_day_count, prev_day_count)
+    diff = base_day_count - prev_day_count
+
+    if base_day_count == 0
+      signal = get_signal(diff)
+      format('本日の新規陽性者数は0人です。（前日比 %s%s人） #covid19 #%s', signal, diff.abs, prefecture)
+    else
+      percent = (diff * 100 / prev_day_count).abs.to_i.to_s + '%'
+      signal = get_signal(diff)
+      format('本日の新規陽性者数は%s人です。（前日比 %s%s人,%s%s） #covid19 #%s', base_day_count, signal, diff.abs, signal, percent, prefecture)
+    end
   end
 end
--- spec/covid_tweet_spec.rb
+++ spec/covid_tweet_spec.rb
@@ -5,10 +5,41 @@ require './covid_tweet.rb'
 RSpec.describe CovidTweet do
   describe '#download' do
     let(:url) { 'https://stopcovid19.metro.tokyo.lg.jp/data/130001_tokyo_covid19_patients.csv' }
-    let(:file_name) { 'test.csv' }
-    it 'returns 0 for an all gutter game' do
+    it 'should not raise error'  do
       covid_tweet = CovidTweet.new
-      expect { covid_tweet.send(:download, url, file_name) }.not_to raise_error
+      expect { covid_tweet.send(:download, url) }.not_to raise_error
+    end
+  end
+  describe '#get_message' do
+    describe 'tokyo' do
+      let(:prefecture) { '東京都' }
+      context '0, 0' do
+        let(:base_day_count) { 0 }
+        let(:prev_day_count) { 0 }
+        let(:output) { '本日の新規陽性者数は0人です。（前日比 +-0人） #covid19 #東京都' }
+
+        it 'should match the response' do
+          expect(CovidTweet.new.send(:get_message, prefecture, base_day_count, prev_day_count)).to eq output
+        end
+      end
+      context '20, 30' do
+        let(:base_day_count) { 20 }
+        let(:prev_day_count) { 30 }
+        let(:output) { '本日の新規陽性者数は20人です。（前日比 -10人,-34%） #covid19 #東京都' }
+
+        it 'should match the response' do
+          expect(CovidTweet.new.send(:get_message, prefecture, base_day_count, prev_day_count)).to eq output
+        end
+      end
+      context '30, 20' do
+        let(:base_day_count) { 30 }
+        let(:prev_day_count) { 20 }
+        let(:output) { '本日の新規陽性者数は30人です。（前日比 +10人,+50%） #covid19 #東京都' }
+
+        it 'should match the response' do
+          expect(CovidTweet.new.send(:get_message, prefecture, base_day_count, prev_day_count)).to eq output
+        end
+      end
     end
   end
 end
