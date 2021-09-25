8ebe4fd1c5925007f466f024ec28e8e593a09aeb
Yuki Matsukura <matsubokkuri@gmail.com>
2021-09-25 21:13:43 +0000

    added cron script

---
+++ today.rb
@@ -0,0 +1,22 @@
+# usage
+#   30,59 9-14 * * * cd /path/to/covid19-daily-tweet && docker-compose run app bundle exec ruby today.rb > /tmp/covid19.log
+require 'bundler/setup'
+Bundler.require
+
+require '/app/lib/covid_tweet_process.rb'
+require 'active_support/all'
+
+# 設定を読み込む
+yaml = YAML.load_file('settings.yaml')
+
+logger = Logger.new(($stdout unless ENV['TEST']))
+
+yaml['accounts'].each do |key, account|
+  begin
+    CovidTweetProcess.new(key, account, 0.day.ago).check_and_publish
+  rescue => e
+    logger.error(key)
+    logger.error(e)
+  end
+end
+
